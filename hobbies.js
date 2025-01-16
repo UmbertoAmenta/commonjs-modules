f7b228decb4d43fb619918f85d199d082ad7149e
Umberto Amenta <umberto.amenta@hotmail.it>
2025-01-16 16:20:54 +0100

    feat: +names, +hobbies, + people

---
+++ hobbies.js
@@ -0,0 +1,10 @@
+// Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+// Esporta la funzione dal file.
+
+function hobbies (hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree]
+}
+
+module.exports = hobbies
+
+console.log(hobbies("coding", "tv", "games"))
---
+++ names.js
@@ -0,0 +1,13 @@
+// Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// Esporta la funzione dal file.
+
+function fullName (firstName, lastName) {
+    return {
+        firstName,
+        lastName,
+    }
+}
+
+module.exports = fullName
+
+console.log(fullName("Mario", "Rossi"))
